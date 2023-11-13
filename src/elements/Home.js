3ebb7e8baaf9a338903bae3ca3fe213c4c18942a
parag <paragh.tibor71@gmail.com>
2023-11-13 16:03:56 +0100

    Create Navbar, HomaPage, install bootstrap

---
+++ src/elements/Home.js
@@ -0,0 +1,13 @@
+import Navbar from './../elements/Navbar';
+import './../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+    return (
+        <>
+            <Navbar />
+            <p className='text-center'>Hello world!! This is a home pgae</p>
+        </>
+    );
+}
+
+export default App;
