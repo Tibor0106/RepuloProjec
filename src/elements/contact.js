e8dd4aa62bc0fd1310359fc2ccdf2eb278fa4ee4
Tibor <112723857+Tibor0106@users.noreply.github.com>
2023-11-15 15:18:18 +0000

    Create Contact page

--- src/elements/contact.js
+++ src/elements/contact.js
@@ -1,6 +1,8 @@
 import Navbar from './../elements/Navbar';
 import './../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+import { motion } from 'framer-motion';
 import { useEffect } from 'react';
 function Contact() {
     useEffect(() => {
@@ -10,17 +12,57 @@ function Contact() {
     return (
         <>
             <Navbar />
-            <div className='container contact'>
-                <div className='row'>
-                    <div className='col-md-5'>
-                        <div className='row'>
-                            <div className='col-md'>
-                                <></>
+            <motion.div
+                initial={{ opacity: 0 }} // Az elem kezdeti átlátszatlansága
+                animate={{ opacity: 1 }} // Az elem animációval történő megjelenítése
+                exit={{ opacity: 0 }} // Az elem kilépésekor alkalmazott animáció
+            >
+                <div className="container contact">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="row mb-3">
+                                <div className="col-sm">
+                                    <div className="input-group mb-3">
+                                        <input className="form-control" type="text" placeholder="Email" />
+                                    </div>
+                                </div>
+                                <div className="col-sm">
+                                    <div className="input-group mb-3">
+                                        <input className="form-control" type="text" placeholder="Név" />
+                                    </div>
+                                </div>
                             </div>
+                            <div className="row mb-3">
+                                <div className="col-sm">
+                                    <div className="input-group mb-3" />
+                                    <input className="form-control" type="text" placeholder="Cím" />
+                                </div>
+                            </div>
+                            <div className="col-sm">
+                                <div className="input-group mb-3">
+                                    <input className="form-control" type="text" placeholder="Telefonszám" />
+                                </div>
+                            </div>
+                            <div className="input-group mb-3">
+                                <textarea className="form-control" placeholder="Írd be az üzeneted">
+                                </textarea>
+                            </div><div className="input-group mb-3">
+                                <button className="btn form-control">Küldés</button>
+                            </div>
+                        </div>
+
+                        <div className="col-md ms-md-5">
+                            <h5>Szívesen hallanánk felőled!</h5>
+                            <h1>Lépj velünk kapcsolatba</h1>
+                            <p>Köszönjük, hogy felkerested weboldalunkat! Ha kérdéseid, észrevételeid vagy javaslataid vannak, örömmel állunk rendelkezésedre. Kérjük, használd az alábbi elérhetőségeket, vagy töltsd ki az alábbi űrl apot, és hamarosan válaszolunk.</p>
+                            <a href="">Kapcsolat</a>
                         </div>
                     </div>
                 </div>
-            </div>
+
+            </motion.div>
+
+
         </>
     );
 }
