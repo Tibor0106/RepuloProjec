9cf715287cde4381915a0c8e97b624cfe387c868
Tibor <112723857+Tibor0106@users.noreply.github.com>
2023-11-17 19:30:22 +0000

    Create SearchPanel, and fix it

--- src/elements/Home.js
+++ src/elements/Home.js
@@ -7,21 +7,6 @@ import SearchPanel from './SearchPanel';
 import { motion } from 'framer-motion';
 
 function Home() {
-    const [messages, setMessages] = useState([]);
-    useEffect(() => {
-        document.title = `EuroJET`;
-        fetch('http://127.0.0.1:3500/destinations/admin')
-            .then(response => response.json())
-            .then(data => {
-                setMessages(data);
-                console.log(data);
-            })
-            .catch(error => {
-                console.error('Hiba a blog üzenetek lekérdezése közben:', error);
-                setMessages([]); // Vagy más hibakezelési logika
-            });
-
-    }, []);
     return (
         <>
             <header>
@@ -40,9 +25,7 @@ function Home() {
             <div className='container info'>
                 <SearchPanel />
             </div>
-            {messages.map(i => (
-                i.destinationName
-            ))}
+
         </>
     );
 }
--- src/elements/SearchPanel.js
+++ src/elements/SearchPanel.js
@@ -1,13 +1,73 @@
-import Navbar from './../elements/Navbar';
-import './../App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap'
-import $ from 'jquery';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { IoMdAirplane } from "react-icons/io";
 import { FaCar } from "react-icons/fa6";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap'
 
 function Panel() {
+    const [destinations, setDestinations] = useState([]);
+    const [searchResultsFrom, setSearchResultsFrom] = useState([]);
+    const [searchResultsTo, setSearchResultsTo] = useState([]);
+    const searchFromRef = useRef(null);
+    const searchToRef = useRef(null);
+
+    useEffect(() => {
+        document.title = `EuroJET`;
+        fetch('http://127.0.0.1:3500/destinations/admin')
+            .then(response => response.json())
+            .then(data => {
+                setDestinations(data);
+            })
+            .catch(error => {
+                console.error('Hiba a blog üzenetek lekérdezése közben:', error);
+                setDestinations([]);
+            });
+
+    }, []);
+
+    const select = (id, name, item) => {
+        if (item == "from") {
+            searchFromRef.current.value = name;
+        } else {
+            searchToRef.current.value = name;
+        }
+
+
+    }
+
+    const searchFrom = () => {
+        console.log("Search from called");
+        const value = searchFromRef.current.value;
+        if (value.length === 0) {
+            setSearchResultsFrom([]);
+            return;
+        }
+
+        var items = [];
+        destinations.forEach(i => {
+            if (i.destinationName.toLowerCase().includes(value.toLowerCase())) {
+                items.push(i);
+            }
+        })
+        setSearchResultsFrom(items);
+    }
+
+    const searchTo = () => {
+        console.log("Search to called");
+        const value = searchToRef.current.value;
+        if (value.length === 0) {
+            setSearchResultsTo([]);
+            return;
+        }
+
+        var items = [];
+        destinations.forEach(i => {
+            if (i.destinationName.toLowerCase().includes(value.toLowerCase())) {
+                items.push(i);
+            }
+        })
+        setSearchResultsTo(items);
+    }
 
     return (
         <>
@@ -36,33 +96,40 @@ function Panel() {
                 </li>
             </ul>
             <div className="tab-content" id="myTabContent">
-                <div className="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+                <div className="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabIndex="0">
                     <div className='row'>
                         <div className='col-md'>
                             <label className='mt-5 text-bold'>Honnan</label>
                             <div className='input-group mb-3'>
-                                <input type='text' className='form-control' />
+                                <input type='text' className='form-control dropdown-toggle' onInput={searchFrom} data-bs-toggle="dropdown" aria-expanded="false" ref={searchFromRef} />
+                                <ul className="dropdown-menu">
+
+                                    {searchResultsFrom.map(i => (
+                                        <li key={i.destinationName}><a className="dropdown-item" onClick={event => select(i.destinationId, i.destinationName, "from")}>{i.destinationName}</a></li>
+                                    ))}
+
+                                    {searchResultsFrom.length === 0 ? "Kezdj el gépelni a kereséshez!" : ""}
+                                </ul>
                             </div>
                         </div>
                         <div className='col-md'>
                             <label className='mt-5 text-bold'>Hova</label>
                             <div className='input-group mb-3'>
-                                <input type='text' className='form-control' />
+                                <input type='text' className='form-control dropdown-toggle' onInput={searchTo} data-bs-toggle="dropdown" aria-expanded="false" ref={searchToRef} />
+                                <ul className="dropdown-menu">
+
+                                    {searchResultsTo.map(i => (
+                                        <li key={i.destinationName}><a className="dropdown-item" onClick={event => select(i.destinationId, i.destinationName, "to")}>{i.destinationName}</a></li>
+                                    ))}
+
+                                    {searchResultsTo.length === 0 ? "Kezdj el gépelni a kereséshez!" : ""}
+                                </ul>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
-                    ...u
-                </div>
-                <div className="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">
-                    ..k.
-                </div>
-                <div className="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab" tabindex="0">
-                    ...k
-                </div>
+                {/* ... */}
             </div>
-
         </>
     );
 }
