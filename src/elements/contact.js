49c82fae897f95251dc0766e6151d3d74392f4e7
MarcellK06 <150803469+MarcellK06@users.noreply.github.com>
2023-11-20 11:00:51 +0100

    changed stub code of "send" to have actual functionality

--- src/elements/contact.js
+++ src/elements/contact.js
@@ -3,12 +3,25 @@ import './../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
 import { motion } from 'framer-motion';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 function Contact() {
+    const email = useRef(null);
+    const name = useRef(null);
+    const address = useRef(null);
+    const phoneNumber = useRef(null);
+    const message = useRef(null);
     useEffect(() => {
 
         document.title = `EuroJET | Kapcsolat`;
     });
+    const send = () => {
+        var v_email = email.current.value;
+        var v_name = name.current.value;
+        var v_address = address.current.value;
+        var v_phoneNumber = phoneNumber.current.value;
+        var v_message = message.current.value;
+        window.location = `http://eurojet.ddns.net:3500/addmessage/${v_email}/${v_name}/${v_address}/${v_phoneNumber}/${v_message}/admin`;
+    }
     return (
         <>
             <Navbar />
@@ -27,31 +40,31 @@ function Contact() {
                             <div className="row">
                                 <div className="col-sm">
                                     <div className="input-group mb-3">
-                                        <input className="form-control" type="text" placeholder="Email" />
+                                        <input className="form-control" type="email" placeholder="Email" ref={email} />
                                     </div>
                                 </div>
                                 <div className="col-sm">
                                     <div className="input-group mb-3">
-                                        <input className="form-control" type="text" placeholder="Név" />
+                                        <input className="form-control" type="text" placeholder="Név" ref={name} />
                                     </div>
                                 </div>
                             </div>
                             <div className="row mb-3">
                                 <div className="col-sm">
                                     <div className="input-group mb-3" />
-                                    <input className="form-control" type="text" placeholder="Cím" />
+                                    <input className="form-control" type="text" placeholder="Cím" ref={address} />
                                 </div>
                             </div>
                             <div className="col-sm">
                                 <div className="input-group mb-3">
-                                    <input className="form-control" type="text" placeholder="Telefonszám" />
+                                    <input className="form-control" type="text" placeholder="Telefonszám" ref={phoneNumber} />
                                 </div>
                             </div>
                             <div className="input-group mb-3">
-                                <textarea className="form-control" placeholder="Írd be az üzeneted">
+                                <textarea className="form-control" placeholder="Írd be az üzeneted" ref={message}>
                                 </textarea>
                             </div><div className="input-group mb-3">
-                                <button className="btn form-control">Küldés</button>
+                                <button className="btn form-control" onClick={send}>Küldés</button>
                             </div>
                         </div>
                         <div className="col-md ms-md-5">
