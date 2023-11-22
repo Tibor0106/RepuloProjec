ed3ee27b45ae4e5ce7c9afe291ed2744f697d6eb
parag <paragh.tibor71@gmail.com>
2023-11-22 16:32:55 +0100

    about base

--- src/elements/about.js
+++ src/elements/about.js
@@ -17,81 +17,37 @@ function About() {
             </header>
 
             <div className='about'>
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ delay: 0.1 }}
-                >
-                    <h1 className='text-uppercase'>Rólunk</h1>
-                </motion.div>
 
             </div>
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ delay: 0.5 }}
-            >
-                <div className='container info'>
-                    <motion.div
-                       initial={{ opacity: 0 }}
-                       animate={{ opacity: 1 }}
-                       transition={{
-                           delay: 0.52
-                       }}
-                    >
-                        <h3>Kik vagyunk mi?</h3>
-                        <div className='mt-3 mb-3'>
-                            <p>Üdvözöljük az EuroJet repülőgépes társaság Európában!</p>
-                            <p>
-                                Az EuroJet büszkén áll a repülés iránti szenvedélyünkkel és az utasok iránti elkötelezettségünkkel. Az Európai égbolton tündöklő csillagként szállunk magasra,
-                                hogy egyedülálló élményeket és kifogástalan szolgáltatást nyújtsunk minden utasunknak.</p>
-                        </div>
-
-                    </motion.div>
-                    <motion.div
-                       initial={{ opacity: 0 }}
-                       animate={{ opacity: 1 }}
-                       transition={{
-                           delay: 0.85
-                       }}
-                    >
-                        <h3>mit kinálunk?</h3>
-                        <div className='mt-3 mb-3'>
-                            <ul>
-                                <li>Autóbérlés</li>
-                                <li>Esetleges <b>kártérítés</b> (Elveszett / sérült csomag)</li>
-                                <li>Utas biztosítás</li>
+            <div className='container info2'>
+                <div className='row'>
+                    <div className='col-ms col-3'>
+                        <div className='_about'>
+                            <ul className='aboutSide'>
+                                <li>Mit kínálunk?</li>
+                            </ul>
+                            <ul className='aboutSide'>
+                                <li className='active'>Kik vagyunk mi?</li>
                             </ul>
                         </div>
-                    </motion.div>
-                    <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1}}
-                        transition={{
-                            delay: 1.16
-                        }}
-                    >
-                        <h3>miért válassz minket?</h3>
-                        <ul>
-                            <ul>
-                                <li>
-                                    <b>Tapasztalat és Hozzáértés:</b> Évek óta a repülés iránti szenvedélyünk vezet bennünket, és tapasztalt csapatunk gondoskodik róla, hogy minden repülés egy különleges élmény legyen.
-                                </li>
-                                <li>
-                                    <b>Biztonság és Megbízhatóság:</b> Az utasaink biztonsága mindennél fontosabb számunkra. Modern repülőgépeinket rendszeresen karbantartjuk, hogy Ön biztonságban repülhessen.
-                                </li>
-                                <li>
-                                    <b>Személyre Szabott Szolgáltatások:</b> Minden ügyfelünk egyedi, és szolgáltatásaink is azok. Velünk repülve személyre szabott élményben lesz része.
-                                </li>
-                            </ul>
+                    </div>
+                    <div className='col-md'>
+                        <div className='about_info'>
+                            <div className='about_title'>
+                                <h3>Kik vagyunk mi?</h3>
+                            </div>
+                            <div className='message'>
+                                <p>
+                                    Üdvözöljük az EuroJet repülőgépes társaság Európában!<br />
 
-                        </ul>
-
-                    </motion.div>
+                                    Az EuroJet büszkén áll a repülés iránti szenvedélyünkkel és az utasok iránti elkötelezettségünkkel. Az Európai égbolton tündöklő csillagként szállunk magasra, hogy egyedülálló élményeket és kifogástalan szolgáltatást nyújtsunk minden utasunknak.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </motion.div>
+            </div>
+
 
 
         </>
